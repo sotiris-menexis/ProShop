7da8371aa33f407acec62f9d6c07cb4054eef089
sotirismenexis <114902532+sotirismenexis@users.noreply.github.com>
2024-12-05 18:14:32 +0200

    Bug fix on server.js

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
 	//set static folder
-	app.user(express.static(path.join(__dirname, "frontend/build")));
+	app.use(express.static(path.join(__dirname, "frontend/build")));
 
 	//any route that is not api will be redirected to index.html
 	app.get("*", (req, res) =>
